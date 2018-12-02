f367eb65610d12ad67a7bc01639c796923feb00d
NKrvavica <nino.krvavica@gmail.com>
2018-12-02 10:42:12 +0100

    day 2, removed unnecessary lines in first_part()

--- Day02/day02.py
+++ Day02/day02.py
@@ -14,19 +14,21 @@ fname = 'input.txt'
 with open(fname) as f:
     lines = f.read().splitlines()
 
+
 def first_part(lines):
     double_letters = 0
     tripple_letters = 0
     for line in lines:
         count = {}
         for s in line:
-          if s in count:
-            count[s] += 1
-          else:
-            count[s] = 1
-        double_tripple = set(count.values())
-        if 2 in double_tripple: double_letters += 1
-        if 3 in double_tripple: tripple_letters += 1
+            if s in count:
+                count[s] += 1
+            else:
+                count[s] = 1
+        if 2 in count.values():
+            double_letters += 1
+        if 3 in count.values():
+            tripple_letters += 1
     return double_letters * tripple_letters
 
 
@@ -38,19 +40,19 @@ def second_part(lines):
     def match(s1, s2):
         """ Checks if two string differ by exactly one character at the same
         position """
-        l = [False, -1]
+        check = [False, -1]
         for i in range(len(s1)):
-            if s1[:i]==s2[:i] and s1[i]!=s2[i] and s1[i+1:]==s2[i+1:]:
-                l = [True, i]
+            if s1[:i] == s2[:i] and s1[i] != s2[i] and s1[i+1:] == s2[i+1:]:
+                check = [True, i]
                 break
-        return l
+        return check
 
     """ Compare each string to every other in the list,
     using itertools.combinations"""
     for line1, line2 in itertools.combinations(lines, 2):
-        l = match(line1, line2)
-        if l[0]:
-            return line1[:l[1]] + line1[l[1]+1:]
+        check = match(line1, line2)
+        if check[0]:
+            return line1[:check[1]] + line1[check[1]+1:]
 
 
 print('common letters:', second_part(lines))
