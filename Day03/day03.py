d351281dc9fdf5665c7731827e34819064dde5e2
NKrvavica <nino.krvavica@gmail.com>
2018-12-03 22:16:21 +0100

    day 3, seperate function for input parse

--- Day03/day03.py
+++ Day03/day03.py
@@ -13,26 +13,22 @@ with open(fname) as f:
     lines = f.read().splitlines()
 
 
-def cut_fabric(lines):
+def parse_input(line):
+    line_nr, rest = line.split(' @ ')
+    start, rectangle = rest.split(': ')
+    x, y = map(int, start.split(','))
+    rec_x, rec_y = map(int, rectangle.split('x'))
+    return line_nr, x, y, rec_x, rec_y
 
+
+def cut_fabric(lines):
     claims = {}
     fabric = np.zeros((1000, 1000))
-
     for line in lines:
-
-        # parse instructions
-        line_nr, rest = line.split(' @ ')
-        start, rectangle = rest.split(': ')
-        x, y = map(int, start.split(','))
-        rec_x, rec_y = map(int, rectangle.split('x'))
-
-        # # fill fabric with ones as instructed in the claim
+        line_nr, x, y, rec_x, rec_y = parse_input(line)
         fabric[x:x+rec_x, y:y+rec_y] += 1
-
-        # generate dictionary (key: claim number, values: coordinates)
         claims[line_nr] = np.array([[x, x+rec_x],
                                    [y, y+rec_y]])
-
     return fabric, claims
 
 
