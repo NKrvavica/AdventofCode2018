baaa29fe062cd740ff6e5ef6a2391d56084de6e1
NKrvavica <nino.krvavica@gmail.com>
2018-12-04 20:20:34 +0100

    day 4, everything solved using dictionary instead of pandas DataFrames

--- Day04/day04.py
+++ Day04/day04.py
@@ -6,9 +6,7 @@ Created on Tue Dec  4 12:06:48 2018
 """
 
 import re
-import collections
 import numpy as np
-import pandas as pd
 from datetime import datetime
 
 fname = 'input.txt'
@@ -17,51 +15,36 @@ with open(fname) as f:
     lines = f.read().splitlines()
 
 
-def sort_inputs(lines):
-    unsorted_input = {}
-    for n, line in enumerate(lines):
-        pd_date = datetime.strptime(line[1:17], '%Y-%m-%d %H:%M')
-        unsorted_input[pd_date] = line
-    sorted_input = collections.OrderedDict(sorted(unsorted_input.items()))
-    return sorted_input
-
-
-sorted_input = sort_inputs(lines)
-
-
-def fill_the_records(sorted_input):
-    column_list = ['ID']
-    column_list.extend(list(range(60)))
-    records = pd.DataFrame(columns=column_list)
-    indx = -1
-    for date, instruc in sorted_input.items():
+def fill_the_records(lines):
+    sorted_input = sorted(lines)
+    records = {}
+    for instruc in sorted_input:
         if instruc[19:24] == 'Guard':
-            indx += 1
             guard_id = re.findall('\d+', instruc[25:])[0]
-            records.loc[indx, 'ID'] = int(guard_id)
+            if guard_id not in records.keys():
+                records[guard_id] = np.zeros(60)
         elif instruc[19:24] == 'falls':
-            startime = date.minute
+            date_time = datetime.strptime(instruc[1:17], '%Y-%m-%d %H:%M')
+            starttime = date_time.minute
         elif instruc[19:24] == 'wakes':
-            endtime = date.minute
-            records.iloc[indx, startime+2:endtime+2] = 1
-    records.fillna(0, inplace=True)
+            date_time = datetime.strptime(instruc[1:17], '%Y-%m-%d %H:%M')
+            endtime = date_time.minute
+            records[guard_id][starttime:endtime] += 1
     return records
 
 
-records = fill_the_records(sorted_input)
+records = fill_the_records(lines)
 
 
 def first_part(records):
     """ which guard sleeps the most """
     max_sleep = 0
-    for i in set(records['ID']):
-        guard_records = records[records['ID'] == i].iloc[:, 2:]
-        minute_at_sleep = guard_records.values.sum(axis=0)
-        if minute_at_sleep.sum() > max_sleep:
-            max_sleep = minute_at_sleep.sum()
-            guard_id = i
-            minute = np.argmax(minute_at_sleep)
-    return guard_id * minute
+    for guard, sleep_time in records.items():
+        if sleep_time.sum() > max_sleep:
+            max_sleep = sleep_time.sum()
+            sleepy_guard = guard
+            minute = np.argmax(sleep_time)
+    return int(sleepy_guard) * minute
 
 
 answer1 = first_part(records)
@@ -71,14 +54,12 @@ print(answer1)
 def second_part(records):
     """ which guard sleeps most frequently at the same time """
     max_frequency = 0
-    for i in set(records['ID']):
-        guard_records = records[records['ID'] == i].iloc[:, 2:]
-        minute_at_sleep = guard_records.values.sum(axis=0)
-        if minute_at_sleep.max() > max_frequency:
-            max_frequency = minute_at_sleep.max()
-            guard_id = i
-            minute = np.argmax(minute_at_sleep)
-    return guard_id * minute
+    for guard, sleep_time in records.items():
+        if sleep_time.max() > max_frequency:
+            max_frequency = sleep_time.max()
+            sleepy_guard = guard
+            minute = np.argmax(sleep_time)
+    return int(sleepy_guard) * minute
 
 
 answer2 = second_part(records)
