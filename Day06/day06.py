b10341427ecd341c64e8d76c6171678ad0caa95e
NKrvavica <nino.krvavica@gmail.com>
2018-12-10 22:43:39 +0100

    day 6, implemented defaultdict instead of dict

--- Day06/day06.py
+++ Day06/day06.py
@@ -7,6 +7,8 @@ Created on Thu Dec  6 07:11:59 2018
 
 import itertools
 import numpy as np
+from collections import defaultdict
+import time
 
 fname = 'input.txt'
 loc = np.loadtxt(fname, delimiter=',')
@@ -18,18 +20,15 @@ def manhattan(x1, y1, x2, y2):
 
 def find_regions(loc):
     max_coord = int(loc.max())
-    region_sizes = {}
+    region_sizes = defaultdict(int)
     max_region_size = 0
     for ix, iy in itertools.product(range(max_coord), range(max_coord)):
         distance = manhattan(ix, iy, loc[:, 0], loc[:, 1])
         # first part
         first_idx, second_idx = distance.argsort()[0:2]
         if distance[first_idx] != distance[second_idx]:
-            if first_idx in region_sizes:
-                region_sizes[first_idx] += 1
-            else:
-                region_sizes[first_idx] = 1
-        if ix==0 or ix==max_coord-1 or iy==0 or iy==max_coord-1:
+            region_sizes[first_idx] += 1
+        if ix == 0 or ix == max_coord-1 or iy == 0 or iy == max_coord-1:
             region_sizes[first_idx] = -999
         # second part
         if distance.sum() < 10_000:
@@ -38,6 +37,9 @@ def find_regions(loc):
     return max_region_1, max_region_size
 
 
+start = time.time()
 a, b = find_regions(loc)
 print('solution to the first part:', a)
 print('solution to the second part:', b)
+end = time.time()
+print(end-start)
